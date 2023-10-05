77dd8240de6bb521226a9c6f91eeeb6283b29e34
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-05 16:32:01 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>Coded by <a 
+    href="https://www.lunasmithart.com/" 
+    target='_blank' 
+    rel="noreferrer" >Luna Smith</a> - <a 
+    href="https://github.com/Lu-Smith/react_headers"  
+    target='_blank' 
+    rel="noreferrer">open source</a></div>
   )
 }
 
