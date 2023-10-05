5af587a0f5a4c1101bd1842fea36385b0f1db64d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-05 20:00:22 +0100

    Created pages

--- src/components/HeaderOne.tsx
+++ src/components/HeaderOne.tsx
@@ -8,7 +8,7 @@ const HeaderOne = () => {
     <ul>
       <li><Link to='/'>Home</Link></li>
       <li><Link to='/about'>About</Link></li>
-      <li><Link to='/shop'>Shop</Link></li>
+      <li><Link to='/shop'>Services</Link></li>
       <li><Link to='/contact'>Contact</Link></li>
     </ul>
   </nav>
