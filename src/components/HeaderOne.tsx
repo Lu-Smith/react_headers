76fae270bd8c3f1f0c8ed226e721eae452fc5543
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-05 19:56:16 +0100

    Added paths to Links

--- src/components/HeaderOne.tsx
+++ src/components/HeaderOne.tsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const HeaderOne = () => {
   return (
     <nav className='HeaderOne'>
-    <div>logo</div>
+    <div><Link to='/'>Logo</Link></div>
     <ul>
-      <li><Link>Home</Link></li>
-      <li><Link>About</Link></li>
-      <li><Link>Shop</Link></li>
-      <li><Link>Contact</Link></li>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/about'>About</Link></li>
+      <li><Link to='/shop'>Shop</Link></li>
+      <li><Link to='/contact'>Contact</Link></li>
     </ul>
   </nav>
   )
