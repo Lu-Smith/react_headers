34a8823466b3690025e7ac94eb71635b2949f8b8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-06 11:19:56 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import HeaderThree from './components/HeaderThree';
 import HeaderFour from './components/HeaderFour';
 import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
+import { About, Services, Contact} from './components/pages';
+import Home from './components/pages/Home';
 
 function App() {
   return (
