6e01bd06e134f59bbb60854af93c61ee0fa85352
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-06 15:01:00 +0100

    Added theme to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import HeaderOne from './components/HeaderOne';
 import HeaderTwo from './components/HeaderTwo';
@@ -10,22 +10,38 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Services from './components/pages/Services';
+//theme
+import { createContext } from 'react';;
+
+interface ThemeProps {
+  theme: string;
+  toggleTheme: () => void;
+};
+
+export const ThemeContext = createContext<null | ThemeProps>(null);
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"))
+  }
   return (
-    <div className="App">
-     <HeaderOne />
-     <HeaderTwo />
-     <HeaderThree />
-     <HeaderFour />
-     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/services' element={<Services />} />
-      <Route path='/contact' element={<Contact />} />
-     </Routes>
-     <Footer />
-    </div>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <div className="App" id={theme}>
+        <HeaderOne />
+        <HeaderTwo />
+        <HeaderThree />
+        <HeaderFour />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
