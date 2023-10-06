811509b91b2b017ccc2c429287335f60d5a8babb
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-06 14:28:47 +0100

    Updated content of HeaderTwo.tsx

--- src/components/HeaderTwo.tsx
+++ src/components/HeaderTwo.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './styles/HeaderTwo.css';
+
 
 const HeaderTwo = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className='HeaderTwo'>
-    <div>logo</div>
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Shop</li>
-      <li>Contact</li>
-    </ul>
-  </div>
+    <nav className='HeaderTwo'>
+      <div className='title'><Link to='/'>Standard</Link></div>
+      <div className="menu" onClick={() => {
+        setMenuOpen(!menuOpen)
+      }}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/about'>About</NavLink></li>
+        <li><NavLink to='/services'>Services</NavLink></li>
+        <li><NavLink to='/contact'>Contact</NavLink></li>
+      </ul>
+    </nav>
   )
 }
 
