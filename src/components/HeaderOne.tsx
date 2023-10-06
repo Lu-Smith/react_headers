f49260b7b6cef0c2f66515459b094e127c3abb97
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-06 11:02:22 +0100

    Updated HeaderOne.tsx

--- src/components/HeaderOne.tsx
+++ src/components/HeaderOne.tsx
@@ -8,7 +8,7 @@ const HeaderOne = () => {
     <ul>
       <li><Link to='/'>Home</Link></li>
       <li><Link to='/about'>About</Link></li>
-      <li><Link to='/shop'>Services</Link></li>
+      <li><Link to='/services'>Services</Link></li>
       <li><Link to='/contact'>Contact</Link></li>
     </ul>
   </nav>
