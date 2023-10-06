bccd28a2361d67a919904e5a71da4bdacb29ea0b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-06 14:17:41 +0100

    Updated HeaderOne.tsx

--- src/components/HeaderOne.tsx
+++ src/components/HeaderOne.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './styles/HeaderOne.css';
 
 const HeaderOne = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className='HeaderOne'>
       <div className='title'><Link to='/'>Logo</Link></div>
-      <div className="manu">
+      <div className="menu">
         <span>Home</span>
         <span>About</span>
         <span>Services</span>
         <span>Contact</span>
       </div>
-      <ul>
+      <ul className={menuOpen ? "open" : ""}>
         <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/about'>About</NavLink></li>
         <li><NavLink to='/services'>Services</NavLink></li>
