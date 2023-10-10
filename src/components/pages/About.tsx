dcc480d532057950f9b6f2a6ec492318b15d7143
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-10 23:07:25 +0100

    Updated About content

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,9 +1,12 @@
-
-import React from 'react'
+import React from 'react';
+import '../styles/About.css';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='About'>
+      <h4>About</h4>
+      <p>Welcome to a showcase of diverse header designs created by Luna Smith. On this page, you'll explore a range of header styles, from elegantly simple to intricately advanced, all crafted using the power of React and CSS. Dive into the world of header design and get inspired by the creative possibilities that await.</p>
+    </div>
   )
 }
 
