574ebac1f0408d698b3815bf87edc23ca79c2370
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-10 23:19:19 +0100

    Updated Contact.tsx content

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Contact.css';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='Contact'>
+      <h4>Check out more of my projects at my portfolio. Please feel free to contact me; I'd love to hear from you and discuss any opportunities or ideas!</h4>
+    </div>
   )
 }
 
